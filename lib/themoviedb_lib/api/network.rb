46575594a09c3f25b0f5b697978e977472de2cea
Diogo Loiola <diogomouraloiola@gmail.com>
2023-02-07 19:07:59 -0300

    create endpoint for network

---
+++ lib/themoviedb_lib/api/network.rb
@@ -0,0 +1,27 @@
+require_relative '../http_request'
+
+module ThemoviedbLib
+  module Api
+    class Network < HttpRequest
+      def initialize(configs)
+        super(configs.base_url, configs.token, '/network')
+      end
+
+      def get
+        raise 'Empty endpoint'
+      end
+
+      def details(id)
+        request(:get, id)
+      end
+
+      def alternative_names(id)
+        request(:get, "/#{id}/alternative_names")
+      end
+
+      def images(id)
+        request(:get, "/#{id}/images")
+      end
+    end
+  end
+end
